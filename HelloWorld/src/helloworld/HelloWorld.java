bcbefc108e49568318a1388feecc933e5df8a7c7
Elke <elke8448@saskpolytech.ca>
2016-12-12 13:47:47 -0600

    Hello world complete

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Brave New World");
     }
     
 }
